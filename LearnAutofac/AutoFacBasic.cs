5469a63576d472b7a8732f9c327c1e5cb4abb079
trquth <tranquthien@gmail.com>
2015-12-13 20:05:38 +0700

    Console App with IoC Autofac
    Console App

---
+++ LearnAutofac/AutoFacBasic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnAutofac
+{
+    interface IAutoFacBasic
+    {
+        void PrintSomething(string text);
+    }
+    class AutoFacBasic:IAutoFacBasic
+    {
+        public void PrintSomething(string text)
+        {
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LearnAutofac/Program.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnAutofac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //registering interface with class that implemented
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AutoFacBasic>().As<IAutoFacBasic>();
+            //resolving interface with autofac
+            var container = builder.Build();
+            var printService = container.Resolve<IAutoFacBasic>();
+            printService.PrintSomething("IoC Autofac framework");
+        }
+    }
+}
